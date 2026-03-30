c9d1521fd90c8c593299fa4a065ea4a2f6bf9e78
Skip <skip@usecurling.com>
2026-03-30 12:11:18 +0000

    feat: Integrate official GoPlay logo across platform

---
+++ dev-dist/assets/loader-circle-jWxocW6q.js
@@ -0,0 +1,8 @@
+import { Br as createLucideIcon } from "./index-CcX5o6Se.js";
+var LoaderCircle = createLucideIcon("loader-circle", [["path", {
+	d: "M21 12a9 9 0 1 1-6.219-8.56",
+	key: "13zald"
+}]]);
+export { LoaderCircle as t };
+
+//# sourceMappingURL=loader-circle-jWxocW6q.js.map
--- dev-dist/assets/minus-BCK9tSqN.js
+++
@@ -1,8 +0,0 @@
-import { Br as createLucideIcon } from "./index-D7HAKqsN.js";
-var Minus = createLucideIcon("minus", [["path", {
-	d: "M5 12h14",
-	key: "1ays0h"
-}]]);
-export { Minus as t };
-
-//# sourceMappingURL=minus-BCK9tSqN.js.map
