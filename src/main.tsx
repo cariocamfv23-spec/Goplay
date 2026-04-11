b15a3cc71b2cd0a351c534a48f5fc4d265c69a5a
Skip <skip@usecurling.com>
2026-04-11 02:10:15 +0000

    Auto-commit after sync

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,4 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './main.css'
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').catch((err) => {
-      console.error('ServiceWorker registration failed: ', err)
-    })
-  })
-}
-
 createRoot(document.getElementById('root')!).render(<App />)
