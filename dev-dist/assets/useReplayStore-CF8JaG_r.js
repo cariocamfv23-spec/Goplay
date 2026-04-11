8d728e996c564c3015e76b223728bf30b3e375c1
Skip <skip@usecurling.com>
2026-04-11 04:29:34 +0000

    feat: Implement PWA with official branding

--- dev-dist/assets/useReplayStore-CF8JaG_r.js
+++
@@ -1,8 +0,0 @@
-import { Ct as mockReplays, zt as create } from "./index-DbWDAVNz.js";
-const useReplayStore = create((set) => ({
-	replays: mockReplays,
-	addReplay: (replay) => set((state) => ({ replays: [replay, ...state.replays] }))
-}));
-export { useReplayStore as t };
-
-//# sourceMappingURL=useReplayStore-CF8JaG_r.js.map
