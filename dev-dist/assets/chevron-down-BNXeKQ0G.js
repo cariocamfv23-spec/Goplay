fc8f9b473a4808c1786ecf07ebb5d5545d28d517
Skip <skip@usecurling.com>
2026-04-13 16:17:25 +0000

    feat: Inject PWA icons and manifest at runtime

--- dev-dist/assets/chevron-down-BNXeKQ0G.js
+++
@@ -1,8 +0,0 @@
-import { Br as createLucideIcon } from "./index-CyB4EoJq.js";
-var ChevronDown = createLucideIcon("chevron-down", [["path", {
-	d: "m6 9 6 6 6-6",
-	key: "qrunsl"
-}]]);
-export { ChevronDown as t };
-
-//# sourceMappingURL=chevron-down-BNXeKQ0G.js.map
--- dev-dist/assets/chevron-left-DIz3YyK_.js
+++
@@ -1,8 +0,0 @@
-import { Br as createLucideIcon } from "./index-CyB4EoJq.js";
-var ChevronLeft = createLucideIcon("chevron-left", [["path", {
-	d: "m15 18-6-6 6-6",
-	key: "1wnfg3"
-}]]);
-export { ChevronLeft as t };
-
-//# sourceMappingURL=chevron-left-DIz3YyK_.js.map
