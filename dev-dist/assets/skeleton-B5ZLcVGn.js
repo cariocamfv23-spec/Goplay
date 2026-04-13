325d142927c6c86f0c073bf0393c5ac29ccc34a4
Skip <skip@usecurling.com>
2026-04-13 15:46:43 +0000

    feat: Implement PWA with vite-plugin-pwa

---
+++ dev-dist/assets/skeleton-B5ZLcVGn.js
@@ -0,0 +1,11 @@
+import { bn as cn, ti as require_jsx_runtime } from "./index-9FdtLN8r.js";
+var import_jsx_runtime = require_jsx_runtime();
+function Skeleton({ className, ...props }) {
+	return /* @__PURE__ */ (0, import_jsx_runtime.jsx)("div", {
+		className: cn("animate-pulse rounded-md bg-muted", className),
+		...props
+	});
+}
+export { Skeleton as t };
+
+//# sourceMappingURL=skeleton-B5ZLcVGn.js.map
