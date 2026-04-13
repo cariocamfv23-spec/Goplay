74ce51b9634811770f883ab33e7200986f8f37be
Skip <skip@usecurling.com>
2026-04-13 15:29:10 +0000

    Auto-commit after sync

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 /* Main entry point for the application - renders the root React component */
 import { createRoot } from 'react-dom/client'
-// @ts-expect-error PWA virtual module managed by plugin
-import { registerSW } from 'virtual:pwa-register'
 import App from './App.tsx'
 import './main.css'
-
-registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')!).render(<App />)
