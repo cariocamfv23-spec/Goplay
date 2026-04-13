3a4304fa6d1b2efd88ff310229c992ea4ea122ea
Skip <skip@usecurling.com>
2026-04-13 15:46:01 +0000

    Auto-commit after sync

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,5 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './main.css'
-import { registerSW } from 'virtual:pwa-register'
-
-registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')!).render(<App />)
