27f494882dd72fd1a4b122ad75de98b33382ad3c
Skip <skip@usecurling.com>
2026-04-13 15:29:00 +0000

    feat: Integrate official PWA brand icon

---
+++ dev-dist/assets/chevron-down-D0vMTnD-.js
@@ -0,0 +1,8 @@
+import { Br as createLucideIcon } from "./index-BdDFDm-L.js";
+var ChevronDown = createLucideIcon("chevron-down", [["path", {
+	d: "m6 9 6 6 6-6",
+	key: "qrunsl"
+}]]);
+export { ChevronDown as t };
+
+//# sourceMappingURL=chevron-down-D0vMTnD-.js.map
---
+++ dev-dist/assets/ellipsis-Ck5u8-TX.js
@@ -0,0 +1,24 @@
+import { Br as createLucideIcon } from "./index-BdDFDm-L.js";
+var Ellipsis = createLucideIcon("ellipsis", [
+	["circle", {
+		cx: "12",
+		cy: "12",
+		r: "1",
+		key: "41hilf"
+	}],
+	["circle", {
+		cx: "19",
+		cy: "12",
+		r: "1",
+		key: "1wjl8i"
+	}],
+	["circle", {
+		cx: "5",
+		cy: "12",
+		r: "1",
+		key: "1pcz8c"
+	}]
+]);
+export { Ellipsis as t };
+
+//# sourceMappingURL=ellipsis-Ck5u8-TX.js.map
