4ebaa6b88d31e08036c0dd4db2cf74b2ffd5a2bb
Skip <skip@usecurling.com>
2026-04-13 15:45:53 +0000

    feat: Implement PWA with vite-plugin-pwa

--- dev-dist/assets/minus-C55KOJGj.js
+++
@@ -1,8 +0,0 @@
-import { Br as createLucideIcon } from "./index-9FdtLN8r.js";
-var Minus = createLucideIcon("minus", [["path", {
-	d: "M5 12h14",
-	key: "1ays0h"
-}]]);
-export { Minus as t };
-
-//# sourceMappingURL=minus-C55KOJGj.js.map
